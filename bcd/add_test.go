b2fd90a9d680b2a1154e91d6234c67e1b5fd56e4
Bruce Ediger <bediger8@gmail.com>
2022-10-05 16:29:11 -0600

    unit test addition

---
+++ bcd/add_test.go
@@ -0,0 +1,72 @@
+package bcd
+
+import (
+	"reflect"
+	"testing"
+)
+
+var one = Number{
+	Sign:     0,
+	Exponent: 0,
+	Digits:   [12]byte{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+}
+var two = Number{
+	Sign:     0,
+	Exponent: 0,
+	Digits:   [12]byte{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+}
+var ten = Number{
+	Sign:     0,
+	Exponent: 1,
+	Digits:   [12]byte{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+}
+var twelve = Number{
+	Sign:     0,
+	Exponent: 1,
+	Digits:   [12]byte{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+}
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		x Number
+		y Number
+	}
+	tests := []struct {
+		name string
+		args args
+		want Number
+	}{
+		{
+			name: "1+1",
+			args: args{x: one, y: one},
+			want: two,
+		},
+		{
+			name: "10+2",
+			args: args{x: ten, y: two},
+			want: twelve,
+		},
+		{
+			name: "2+10",
+			args: args{x: two, y: ten},
+			want: twelve,
+		},
+		{
+			name: "12+0",
+			args: args{x: twelve, y: Number{}},
+			want: twelve,
+		},
+		{
+			name: "0+12",
+			args: args{x: Number{}, y: twelve},
+			want: twelve,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.x, tt.args.y); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
