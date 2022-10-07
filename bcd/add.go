9b764cd33d57060ed888041fea9184acdb372b3a
Bruce Ediger <bediger8@gmail.com>
2022-10-07 14:47:00 -0600

    hand off to Sub, normalize 0 Numbers

--- bcd/add.go
+++ bcd/add.go
@@ -4,6 +4,28 @@ const scratchSize = 24
 
 // Add creates a BCD number by adding 2 BCD numbers
 func Add(x, y Number) Number {
+
+	// (+x) + (-y)
+	if x.Sign == 0 && y.Sign == 1 {
+		y.Sign = 0
+		return Sub(x, y)
+	}
+	// (-x) + (-y)
+	if x.Sign == 1 && y.Sign == 1 {
+		x.Sign = 0
+		y.Sign = 0
+		answer := Add(x, y)
+		answer.Sign = 1
+		return answer
+	}
+	// (-x) + (+y)
+	if x.Sign == 1 && y.Sign == 1 {
+		y.Sign = 0
+		return Sub(y, x)
+	}
+
+	// (+x) + (+y) from here on out
+
 	var scratch [scratchSize]byte
 
 	big := x
@@ -12,6 +34,7 @@ func Add(x, y Number) Number {
 		big = y
 		small = x
 	}
+
 	for i := 0; i < 12; i++ {
 		scratch[i] = big.Digits[i]
 	}
@@ -42,12 +65,19 @@ func Add(x, y Number) Number {
 		sum.Exponent = 1
 	}
 
+	nonZeroDigitCounts := 0
 	for j := 0; j < 12 && i < 12; j++ {
+		if scratch[i] != 0 {
+			nonZeroDigitCounts++
+		}
 		sum.Digits[i] = scratch[j]
 		i++
 	}
 
 	sum.Exponent += big.Exponent
+	if nonZeroDigitCounts == 0 {
+		sum.Exponent = 0
+	}
 
 	return sum
 }
